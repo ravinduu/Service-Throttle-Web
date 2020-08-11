c231c8f5a50520a0c5e6b2a982c2d6fbff84821e
ravdsn <ravindu.dilshan.rd@gmail.com>
2020-08-11 21:03:39 +0530

    18 add two tests

--- st-uaa-service/src/test/java/com/servicethrottle/stuaaservice/StUaaServiceApplicationTests.java
+++ st-uaa-service/src/test/java/com/servicethrottle/stuaaservice/StUaaServiceApplicationTests.java
@@ -1,13 +1,25 @@
 package com.servicethrottle.stuaaservice;
 
+import com.servicethrottle.stuaaservice.controllers.AccountController;
+import com.servicethrottle.stuaaservice.controllers.UserAuthController;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class StUaaServiceApplicationTests {
 
+	@Autowired
+	private AccountController accountController;
+
+	@Autowired
+	private UserAuthController userAuthController;
+
 	@Test
-	void contextLoads() {
+	void contextLoads() throws Exception{
+		Assertions.assertThat(accountController).isNotNull();
+		Assertions.assertThat(userAuthController).isNotNull();
 	}
 
 }
