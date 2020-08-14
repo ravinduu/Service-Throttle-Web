0c62c975a0bbff7d5de1db98f6530aba23fef794
ravdsn <ravindu.dilshan.rd@gmail.com>
2020-08-14 23:33:20 +0530

    26 add custom exception for not found customer by email

---
+++ st-uaa-service/src/main/java/com/servicethrottle/stuaaservice/exceptions/NoAccountByThisEmailException.java
@@ -0,0 +1,7 @@
+package com.servicethrottle.stuaaservice.exceptions;
+
+public class NoAccountByThisEmailException extends RuntimeException {
+    public NoAccountByThisEmailException(){
+
+    }
+}
