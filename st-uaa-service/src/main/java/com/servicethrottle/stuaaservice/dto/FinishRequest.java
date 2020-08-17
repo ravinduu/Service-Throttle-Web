7daa8f92e188a3cbb087c5febca27f04ad9f279d
ravdsn <ravindu.dilshan.rd@gmail.com>
2020-08-17 12:25:53 +0530

    36 edit FinishRequest dto

--- st-uaa-service/src/main/java/com/servicethrottle/stuaaservice/dto/FinishRequest.java
+++ st-uaa-service/src/main/java/com/servicethrottle/stuaaservice/dto/FinishRequest.java
@@ -7,28 +7,44 @@ import javax.validation.constraints.Size;
 
 public class FinishRequest {
 
+    @NotNull
+    @Size(min = 5, max = 100)
+    private String custUsername;
+
+    @NotNull
     @Size(max = 50)
     private String custFirstName;
 
+    @NotNull
     @Size(max = 50)
     private String custLastName;
 
+    @NotNull
     @Size(min = 9, max = 12)
     private String custPhoneNumber;
 
-
+    @NotNull
     @Size(max = 254)
-    @Column(length = 254)
     private String custAddress;
 
-    public FinishRequest(@Size(max = 50) String custFirstName,
-                         @Size(max = 50) String custLastName,
-                         @Size(min = 9, max = 12) String custPhoneNumber,
-                         @Size(max = 254) String custAddress) {
+    public FinishRequest(@NotNull @Size(min = 5, max = 100) String custUsername,
+                         @NotNull @Size(max = 50) String custFirstName,
+                         @NotNull @Size(max = 50) String custLastName,
+                         @NotNull @Size(min = 9, max = 12) String custPhoneNumber,
+                         @NotNull @Size(max = 254) String custAddress) {
+        this.custUsername = custUsername;
         this.custFirstName = custFirstName;
         this.custLastName = custLastName;
         this.custPhoneNumber = custPhoneNumber;
         this.custAddress = custAddress;
+    }
+
+    public String getCustUsername() {
+        return custUsername;
+    }
+
+    public void setCustUsername(String custUsername) {
+        this.custUsername = custUsername;
     }
 
     public String getCustFirstName() {
@@ -61,5 +77,16 @@ public class FinishRequest {
 
     public void setCustAddress(String custAddress) {
         this.custAddress = custAddress;
+    }
+
+    @Override
+    public String toString() {
+        return "FinishRequest{" +
+                "custUsername='" + custUsername + '\'' +
+                ", custFirstName='" + custFirstName + '\'' +
+                ", custLastName='" + custLastName + '\'' +
+                ", custPhoneNumber='" + custPhoneNumber + '\'' +
+                ", custAddress='" + custAddress + '\'' +
+                '}';
     }
 }
