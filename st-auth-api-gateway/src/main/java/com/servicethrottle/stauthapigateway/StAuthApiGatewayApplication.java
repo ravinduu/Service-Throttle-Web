ca097dc2710f031826e68e4caade06fd08cec310
ravdsn <ravindu.dilshan.rd@gmail.com>
2020-08-18 15:20:50 +0530

    2 create eureka client

--- st-auth-api-gateway/src/main/java/com/servicethrottle/stauthapigateway/StAuthApiGatewayApplication.java
+++ st-auth-api-gateway/src/main/java/com/servicethrottle/stauthapigateway/StAuthApiGatewayApplication.java
@@ -2,8 +2,10 @@ package com.servicethrottle.stauthapigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class StAuthApiGatewayApplication {
 
 	public static void main(String[] args) {
