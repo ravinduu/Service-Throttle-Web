1d52d89cafc4b0b95acfaf976738b02214177813
ravdsn <ravindu.dilshan.rd@gmail.com>
2020-08-18 16:47:07 +0530

    4 enable web security and jwt dependencies

--- st-auth-api-gateway/src/main/java/com/servicethrottle/stauthapigateway/StAuthApiGatewayApplication.java
+++ st-auth-api-gateway/src/main/java/com/servicethrottle/stauthapigateway/StAuthApiGatewayApplication.java
@@ -3,8 +3,10 @@ package com.servicethrottle.stauthapigateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
+@EnableWebSecurity
 @EnableEurekaClient
 public class StAuthApiGatewayApplication {
 
