cf6432d0aada0be3fd5436b3c7cc64cdc17b8102
ravdsn <ravindu.dilshan.rd@gmail.com>
2020-08-18 12:09:17 +0530

    1 create authentication and gateway service

---
+++ st-auth-api-gateway/src/main/java/com/servicethrottle/stauthapigateway/StAuthApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.servicethrottle.stauthapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StAuthApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StAuthApiGatewayApplication.class, args);
+	}
+
+}
