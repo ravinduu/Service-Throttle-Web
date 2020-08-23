bba3740e136f6397e08c66f3b38e29eed69c8f8f
ravdsn <ravindu.dilshan.rd@gmail.com>
2020-08-23 20:18:50 +0530

    9 add properties for zuul

--- st-auth-api-gateway/src/main/java/com/servicethrottle/stauthapigateway/StAuthApiGatewayApplication.java
+++ st-auth-api-gateway/src/main/java/com/servicethrottle/stauthapigateway/StAuthApiGatewayApplication.java
@@ -3,11 +3,13 @@ package com.servicethrottle.stauthapigateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
 @EnableWebSecurity
 @EnableEurekaClient
+@EnableZuulProxy
 public class StAuthApiGatewayApplication {
 
 	public static void main(String[] args) {
