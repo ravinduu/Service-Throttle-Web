9b8c2ced34e9b6f5077f43cbfc3b70ca85a64b54
ravdsn <ravindu.dilshan.rd@gmail.com>
2020-09-26 20:33:17 +0530

    rename directory

---
+++ Service-Throttle-Backend/src/main/java/com/servicethrottle/servicethrottlebackend/ServiceThrottleBackendApplication.java
@@ -0,0 +1,13 @@
+package com.servicethrottle.servicethrottlebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceThrottleBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceThrottleBackendApplication.class, args);
+	}
+
+}
