6420c43dc104c200e9d0cc1f3bd6b5ed5e8a7b20
ravdsn <ravindu.dilshan.rd@gmail.com>
2020-11-24 22:30:02 +0530

    minor change

--- Service-Throttle-Backend/service-throttle-backend/src/main/java/com/servicethrottle/servicethrottlebackend/services/SetupAuthoritiesAndAdminAccount.java
+++ Service-Throttle-Backend/service-throttle-backend/src/main/java/com/servicethrottle/servicethrottlebackend/services/SetupAuthoritiesAndAdminAccount.java
@@ -2,17 +2,13 @@ package com.servicethrottle.servicethrottlebackend.services;
 
 import com.servicethrottle.servicethrottlebackend.models.Admin;
 import com.servicethrottle.servicethrottlebackend.models.Authority;
-import com.servicethrottle.servicethrottlebackend.models.Customer;
 import com.servicethrottle.servicethrottlebackend.models.UserAuthenticationCredentials;
-import com.servicethrottle.servicethrottlebackend.models.enums.AuthorityType;
+import com.servicethrottle.servicethrottlebackend.models.dto.RegistrationRequest;
 import com.servicethrottle.servicethrottlebackend.repositories.AdminRepository;
-import com.servicethrottle.servicethrottlebackend.repositories.AuthorityRepository;
-import com.servicethrottle.servicethrottlebackend.repositories.CustomerRepository;
 import com.servicethrottle.servicethrottlebackend.repositories.UserAuthenticationCredentialsRepository;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Set;
 
@@ -22,73 +18,56 @@ import static com.servicethrottle.servicethrottlebackend.models.enums.AuthorityT
 public class SetupAuthoritiesAndAdminAccount implements ApplicationListener<ContextRefreshedEvent> {
 
     private boolean alreadySetup = false;
-    private final AdminRepository adminRepository;
-    private final AuthorityRepository authorityRepository;
-    private final UserAuthenticationCredentialsRepository UACRepo;
-    private final CustomerRepository customerRepository;
 
-    public SetupAuthoritiesAndAdminAccount(AdminRepository adminRepository, AuthorityRepository authorityRepository, UserAuthenticationCredentialsRepository UACRepo, CustomerRepository customerRepository) {
+    private final UserAuthenticationCredentialsRepository UACRepo;
+
+    private final AdminRepository adminRepository;
+    private final AuthorityService authorityService;
+    private final UserAccountService userAccountService;
+
+
+    public SetupAuthoritiesAndAdminAccount(AdminRepository adminRepository,
+                                           AuthorityService authorityService,
+                                           UserAuthenticationCredentialsRepository UACRepo,
+                                           UserAccountService userAccountService) {
         this.adminRepository = adminRepository;
-        this.authorityRepository = authorityRepository;
+        this.authorityService = authorityService;
         this.UACRepo = UACRepo;
-        this.customerRepository = customerRepository;
+        this.userAccountService = userAccountService;
     }
 
     @Override
     public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
         if(alreadySetup) return;
 
-        Authority adminAuthority =  createAuthorityIfNotFound(ADMIN);
-        Authority supervisorAuthority = createAuthorityIfNotFound(SUPERVISOR);
-        Authority mobileMechanicAuthority = createAuthorityIfNotFound(MOBILE_MECHANIC);
-        Authority customerAuthority = createAuthorityIfNotFound(CUSTOMER);
+        Authority adminAuthority =  authorityService.createAuthorityIfNotFound(ADMIN.getAuthorityType());
+        Authority supervisorAuthority = authorityService.createAuthorityIfNotFound(SUPERVISOR.getAuthorityType());
+        Authority mobileMechanicAuthority = authorityService.createAuthorityIfNotFound(MOBILE_MECHANIC.getAuthorityType());
+        Authority customerAuthority = authorityService.createAuthorityIfNotFound(CUSTOMER.getAuthorityType());
 
         Admin superAdmin = new Admin();
         UserAuthenticationCredentials UAC = new UserAuthenticationCredentials();
 
         superAdmin.setUsername("admin");
         superAdmin.setEmail("admin@admin.com");
-        Set<Authority> superAdminAuthorities = UAC.getAuthorities();
-        superAdminAuthorities.add(adminAuthority);
-        superAdminAuthorities.add(adminAuthority);
         superAdmin.setSuperAdmin(true);
         superAdmin.setActivated(true);
 
         UAC.setUsername("admin");
         UAC.setPassword("admin");
+        Set<Authority> superAdminAuthorities = UAC.getAuthorities();
+        superAdminAuthorities.add(adminAuthority);
+        superAdminAuthorities.add(supervisorAuthority);
+        superAdminAuthorities.add(mobileMechanicAuthority);
+        superAdminAuthorities.add(customerAuthority);
         UAC.setAuthorities(superAdminAuthorities);
 
         UACRepo.save(UAC);
         adminRepository.save(superAdmin);
 
-        Customer newCustomer = new Customer();
-        UserAuthenticationCredentials UACOne = new UserAuthenticationCredentials();
-        newCustomer.setUsername("customer");
-        newCustomer.setEmail("customer@customer.com");
-
-        Set<Authority> customerAuthorities = UACOne.getAuthorities();
-        customerAuthorities.add(customerAuthority);
-        newCustomer.setActivated(true);
-
-        UACOne.setUsername("customer");
-        UACOne.setPassword("customer");
-        UACOne.setAuthorities(customerAuthorities);
-        customerRepository.save(newCustomer);
-        UACRepo.save(UACOne);
-
+        RegistrationRequest registrationRequest = new RegistrationRequest();
+        userAccountService.registerUser(registrationRequest);
         alreadySetup = true;
-
     }
 
-    @Transactional
-    Authority createAuthorityIfNotFound(AuthorityType authorityType) {
-
-        Authority authority = authorityRepository.findByAuthority(authorityType);
-        if (authority == null) {
-            authority = new Authority();
-            authority.setAuthority(authorityType);
-            authorityRepository.save(authority);
-        }
-        return authority;
-    }
 }
