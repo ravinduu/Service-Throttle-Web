8eb35b0fe98dc811d754dd53e45d7c9a39e6c31e
ravdsn <ravindu.dilshan.rd@gmail.com>
2020-11-28 18:31:29 +0530

    renamed

--- Service-Throttle-Backend/service-throttle-backend/src/main/java/com/servicethrottle/servicethrottlebackend/models/dto/LoginRequestDto.java
+++ Service-Throttle-Backend/service-throttle-backend/src/main/java/com/servicethrottle/servicethrottlebackend/models/dto/LoginRequestDto.java
@@ -1,18 +1,16 @@
 package com.servicethrottle.servicethrottlebackend.models.dto;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.validation.constraints.Email;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class RegistrationRequest {
+public class LoginRequestDto {
     @NotNull
     @Size(min = 5, max = 100)
     private String username;
@@ -20,14 +18,4 @@ public class RegistrationRequest {
     @NotNull
     @Size(min = 8)
     private String password;
-
-    @NotNull
-    @Email
-    @Size(min = 5, max = 254)
-    private String email;
-
-    @NotNull
-    private String accountType;
-
-
 }
--- Service-Throttle-Backend/service-throttle-backend/src/main/java/com/servicethrottle/servicethrottlebackend/models/dto/RegistrationRequestDto.java
+++ Service-Throttle-Backend/service-throttle-backend/src/main/java/com/servicethrottle/servicethrottlebackend/models/dto/RegistrationRequestDto.java
@@ -1,16 +1,18 @@
 package com.servicethrottle.servicethrottlebackend.models.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class LoginRequest {
+public class RegistrationRequestDto {
     @NotNull
     @Size(min = 5, max = 100)
     private String username;
@@ -18,4 +20,14 @@ public class LoginRequest {
     @NotNull
     @Size(min = 8)
     private String password;
+
+    @NotNull
+    @Email
+    @Size(min = 5, max = 254)
+    private String email;
+
+    @NotNull
+    private String accountType;
+
+
 }
