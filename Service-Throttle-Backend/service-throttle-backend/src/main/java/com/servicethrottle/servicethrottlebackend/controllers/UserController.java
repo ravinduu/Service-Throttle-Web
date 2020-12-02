9869c2f363d581575da05220c6e5801ea13a8cfd
ravdsn <ravindu.dilshan.rd@gmail.com>
2020-12-02 20:26:55 +0530

    implement get user details by username

--- Service-Throttle-Backend/service-throttle-backend/src/main/java/com/servicethrottle/servicethrottlebackend/controllers/UserController.java
+++ Service-Throttle-Backend/service-throttle-backend/src/main/java/com/servicethrottle/servicethrottlebackend/controllers/UserController.java
@@ -10,7 +10,9 @@ import lombok.AllArgsConstructor;
 import org.dom4j.util.UserDataElement;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -52,7 +54,7 @@ public class UserController {
         return ResponseEntity.ok().body(supervisorService.getAllSupervisors());
     }
 
-    @GetMapping("/users/mobile_mechanic")
+    @GetMapping("/users/mobile-mechanic")
     @PreAuthorize("hasAnyAuthority('ROLE_ADMIN','ROLE_SUPERVISOR')")
     public ResponseEntity<List<UserDetailsDto>> getAllMobileMechanics(){
         return ResponseEntity.ok().body(mobileMechanicService.getAllMobileMechanics());
@@ -64,5 +66,56 @@ public class UserController {
         return ResponseEntity.ok().body(customerService.getAllCustomers());
     }
 
-    
+    /**
+     * get details of a admin
+     *
+     * parameter username use to fetch info from db
+     * ROLE_ADMIN requires
+     * return {userDetailsDto} details of admin
+     * */
+    @GetMapping("/user/admin/{username}")
+    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
+    public ResponseEntity<UserDetailsDto> getAdmin(@PathVariable String username){
+        return ResponseEntity.ok().body(adminService.getAdmin(username));
+    }
+
+    /**
+     * get details of a supervisor
+     *
+     * parameter username use to fetch info from db
+     * ROLE_ADMIN requires
+     * return {userDetailsDto} details of supervisor
+     * */
+    @GetMapping("/user/supervisor/{username}")
+    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
+    public ResponseEntity<UserDetailsDto> getSupervisor(@PathVariable String username){
+        return ResponseEntity.ok().body(supervisorService.getSupervisor(username));
+    }
+
+    /**
+     * get details of a mobile-mechanic
+     *
+     * parameter username use to fetch info from db
+     * ROLE_ADMIN, ROLE_SUPERVISOR, ROLE_MM or ROLE_CUSTOMER requires
+     * return {userDetailsDto} details of MM
+     * */
+    @GetMapping("/user/mobile-mechanic/{username}")
+    @PreAuthorize("hasAnyAuthority('ROLE_ADMIN','ROLE_SUPERVISOR','ROLE_MOBILEMECHANIC','ROLE_CUSTOMER')")
+    public ResponseEntity<UserDetailsDto> getMobileMechanic(@PathVariable String username){
+        return ResponseEntity.ok().body(mobileMechanicService.getMobileMechanic(username));
+    }
+
+    /**
+     * get details of a customer
+     *
+     * parameter username use to fetch info from db
+     * ROLE_ADMIN, ROLE_SUPERVISOR or ROLE_MM requires
+     * return {userDetailsDto} details of customer
+     * */
+    @GetMapping("/user/customer/{username}")
+    @PreAuthorize("hasAnyAuthority('ROLE_ADMIN','ROLE_SUPERVISOR','ROLE_MOBILEMECHANIC')")
+    public ResponseEntity<UserDetailsDto> getCustomer(@PathVariable String username){
+        return ResponseEntity.ok().body(customerService.getCustomer(username));
+    }
+
 }
