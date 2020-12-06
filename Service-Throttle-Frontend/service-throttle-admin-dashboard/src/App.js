4e548556179a45217a1d9fc3b46576b052194006
ravdsn <ravindu.dilshan.rd@gmail.com>
2020-12-06 20:33:38 +0530

    create frontend admin dashboard

---
+++ Service-Throttle-Frontend/service-throttle-admin-dashboard/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import React from 'react';
+import {Login} from "./components/login/index"
+
+function App() {
+  return (
+    <div className="App">
+        <Login/>
+    </div>
+  );
+}
+
+export default App;
