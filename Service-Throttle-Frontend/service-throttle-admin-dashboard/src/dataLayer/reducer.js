4001b3bd2f33d780604c90af7a4983c16e1c08e4
ravdsn <ravindu.dilshan.rd@gmail.com>
2021-01-12 21:28:45 +0530

    implement datalayer with reducer

---
+++ Service-Throttle-Frontend/service-throttle-admin-dashboard/src/dataLayer/reducer.js
@@ -0,0 +1,24 @@
+export const initialState = {
+  user: null,
+  token: null,
+};
+
+export const reducer = (state, action) => {
+  console.log(action);
+  switch (action.type) {
+    case "SET_USER":
+      return {
+        ...state,
+        user: action.user,
+      };
+
+    case "SET_TOKEN":
+      return {
+        ...state,
+        token: action.token,
+      };
+
+    default:
+      return state;
+  }
+};
