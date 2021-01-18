3821288b9233d4a4682ec47298cf226ec911a70d
ravdsn <ravindu.dilshan.rd@gmail.com>
2021-01-18 22:15:51 +0530

    minor changes

--- Service-Throttle-Frontend/service-throttle-admin-dashboard/src/components/navbar/NavBar.jsx
+++ Service-Throttle-Frontend/service-throttle-admin-dashboard/src/components/navbar/NavBar.jsx
@@ -4,7 +4,7 @@ import { useDataLayerValue } from "../../dataLayer/DataLayer";
 function NavBar() {
   const [{ user }] = useDataLayerValue();
 
-  return <div>{user ? <h1> {user.email} </h1> : <h1>dashboard</h1>}</div>;
+  return <div>{user ? <h1> {user.email} </h1> : <h1>Navbar</h1>}</div>;
 }
 
 export default NavBar;
