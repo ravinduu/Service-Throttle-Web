9921c691fde3dba82429a4ee25636ebd979edc78
ravdsn <ravindu.dilshan.rd@gmail.com>
2021-02-24 14:22:31 +0530

    implement contexts

---
+++ Service-Throttle-Frontend/service-throttle-app/ServiceThrottle/context/reducer.js
@@ -0,0 +1,24 @@
+export const initialState = {
+  user: null,
+  token: null,
+  api: "https://servicethrottle.herokuapp.com/st",
+};
+
+export const reducer = (state, action) => {
+  switch (action.type) {
+    case "SET_USER":
+      return {
+        ...state,
+        user: action.user,
+      };
+
+    case "SET_TOKEN":
+      return {
+        ...state,
+        token: action.token,
+      };
+
+    default:
+      return state;
+  }
+};
